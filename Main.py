a0ee2e9c898b10c9fbe19f1c9f9f747617486b3f
Phoenix Sean <seanstan95@gmail.com>
2020-02-11 07:24:49 -0500

    Parsing Fixes - Completely overhauled the approach to parsing, and most work now (see README for more detail). - Various adjustments to arguments and functions to fit the new approach. - Added more documentation to functions.

--- Main.py
+++ Main.py
@@ -1,15 +1,12 @@
 import sqlite3
+from bs4 import BeautifulSoup
 import requests
 
 
 def open_city_file():
+    """Opens the cities file and reads the contents into a list to be compared to with job comments."""
     city_file = open("cities.txt")
-    cities = []
-
-    for line in city_file:
-        cities.append(line.lower())
-
-    return cities
+    return city_file.read().splitlines()
 
 
 def connect_db(db_name: str):
@@ -31,15 +28,18 @@ def close_db(conn: sqlite3.Connection):
     conn.close()
 
 
-def get_job_ids(url: str):
-    """Pulls the top level comment from Ask HN, and retrieves the ID of the job postings in the thread."""
+def request_comment(url: str, tag: str):
+    """Makes a request to the API for a specific comment, and attempts to pull a tag out. This is generalized to allow
+    for either 'kids' or 'text' to be pulled. In the case of deleted comments and there is no 'text' value, exceptions
+    handle what to do with them."""
     try:
         response = requests.get(url)  # top level Ask HN post
+        try:
+            return response.json()[tag]
+        except (TypeError, KeyError):
+            return "Null Response or Invalid Key"
     except requests.exceptions.ConnectionError:
         return "Invalid URL"
-
-    if response is not None:
-        return response.json()["kids"]  # job listings are top level children of initial post
 
 
 def check_ids(cursor: sqlite3.Cursor, job_ids: list):
@@ -53,116 +53,120 @@ def check_ids(cursor: sqlite3.Cursor, job_ids: list):
 
 
 def get_listings(job_ids: list):
-    job_listings = []  # list of lists, each entry here is a list of its id and its description
+    """Handles making requests for each of the job comment IDs passed in via job_ids. Saves each comment's ID and
+    its text description into a list of lists job_listings."""
+    job_listings = []  # list of lists, each entry is a list of its id and its description
     count = 0
 
     for job in job_ids:
-        listing = requests.get("https://hacker-news.firebaseio.com/v0/item/" + str(job) + ".json").json()
-        print("Processing job #" + str(count) + "/" + str(len(job_ids)))
+        listing = request_comment("https://hacker-news.firebaseio.com/v0/item/" + str(job) + ".json", "text")
         count += 1
-        if listing is not None and listing.get("text") is not None:
-            text = listing.get("text")
-            job_listings.append([job, text])  # appends ID and then text
+        if listing == "Null Response or Invalid Key":
+            print("Skipping invalid/null comment (" + str(count) + "/" + str(len(job_ids)) + ").")
+            continue
+
+        print("Processing job (" + str(count) + "/" + str(len(job_ids)) + ").")
+        job_listings.append([job, listing])  # appends ID and then text
 
     return job_listings
 
 
 def parse_listings(job_listings: list, cities: list):
+    """Parses each job for: ID, title, location, skills, visa, remote/onsite status, website, and description."""
     parsed = []  # list of lists, each entry here is a list of its parsed elements
 
     for job in job_listings:
-        count = 0
-        checks = [False] * 6
-        data = [job[0], "Unknown Title", "Unknown Location", "Unknown Skills", "Unknown Visa Sponsorship",
-                "Unknown Remote/Onsite", "Unknown Website", "Unknown Description"]  # default values
+        data = list()
 
-        # setup new entry and append ID to start
-        split = job[1].split('|')
+        # ID: job[0] always contains the ID of comment; referenced to avoid requesting jobs that are already in database
+        data.append(job[0])
 
-        for part in split:
-            count += 1
-            if count == len(split)-1:
-                data[7] = split[-1]
-                break
+        # Title: assumes first word; not all are one word but this ensures retrieval of *some* data for every job entry
+        data.append(job[1].lstrip().split(' ', 1)[0])  # gets the first word (stripped of whitespace)
 
-            # title
-            if checks[0] is False:
-                checks[0] = True
-                data[1] = part  # assuming first split entry is always title for now
-                continue
+        # Location: check if any of the cities list exists in job comment
+        data.append(check_location(job[1], cities))
 
-            # location
-            if check_city(checks, data, part, cities):
-                continue
+        # Skills TODO: not sure how to identify skills yet, fall back to a default value for now
+        data.append("Unknown Skills")
 
-            if check_job(checks, 2, data, "skills", part, part):
-                continue
+        # Visa: check if the keyword 'visa' exists in job comment
+        data.append(check_visa(job[1]))
 
-            if check_job(checks, 3, data, "visa", "Yes", part):
-                continue
+        # Remote/Onsite: check if keywords 'remote', 'onsite', or both exist in job comment
+        data.append(check_remote(job[1]))
 
-            if check_remote(checks, data, part):
-                continue
+        # Website: uses BeautifulSoup to parse any <a href='website-name.com'> tags from job comment
+        data.append(check_website(job[1]))
 
-            if check_job(checks, 5, data, "http", part, part):
-                continue
+        # Description: insert the full job comment (stripped of HTML tags for cleanliness sake)
+        data.append(BeautifulSoup(job[1], 'html.parser').get_text())
 
         parsed.append(data)
 
     return parsed
 
 
-def check_city(checks: list, data: list, part: str, cities: list):
-    if checks[1] is False:
-        for city in cities:
-            if city in part.lower():
-                checks[1] = True
-                data[2] = part
-                return True
+def check_remote(job: str):
+    """Helper function for checking whether or not a job supports remote or only onsite working (or both)."""
+    if "remote" in job.lower() and "onsite" in job.lower():
+        return "Remote and Onsite"
+    elif "remote" in job.lower():
+        return "Remote"
+    elif "onsite" in job.lower():
+        return "Onsite"
+    else:
+        return "Unknown Remote/Onsite"  # if not found
 
 
-def check_job(checks: list, num: int, data: list, check_value: str, add_value: str, part: str):
-    if checks[num] is False and check_value in part.lower():
-        checks[num] = True
-        data[num+1] = add_value
-        return True
+def check_location(job: str, cities: list):
+    """Helper function for checking if the location of a job is listed in the city list retrieved from cities.txt."""
+    for city in cities:
+        if " " + city.lower() + " " in job.lower() or " " + city.lower() + ", " in job.lower():
+            return city
+    return "Unknown Location"  # if not found
 
 
-def check_remote(checks: list, data: list, part: str):
-    if checks[4] is False:
-        if "remote" in part.lower() and "onsite" in part.lower():
-            data[5] = "Remote and Onsite"
-            checks[4] = True
-            return True
-        elif "remote" in part.lower():
-            data[5] = "Remote"
-            checks[4] = True
-            return True
-        elif "onsite" in part.lower():
-            data[5] = "Onsite"
-            checks[4] = True
-            return True
+def check_visa(job: str):
+    """Helper function for checking if a job supports visa sponsorships or not."""
+    if "visa" in job.lower():
+        return "Yes"
+    else:
+        return "No"
 
 
-def write_db(cursor: sqlite3.Cursor, table: str, values: list, replace: str):
+def check_website(job: str):
+    """Helper function for checking if a job has a website posted in it. Uses BeautifulSoup to find all <a href> tags
+    and returns their values."""
+    soup = BeautifulSoup(job, 'html.parser')
+    tags = soup.find_all('a')
+    if len(tags) > 0:
+        return tags[0].get('href')
+    else:
+        return "Unknown Website"
+
+
+def write_db(cursor: sqlite3.Cursor, statement: str, values: list):
+    """Writes data to the database and ensures that only good data goes in, and bad data gets rejected with
+    an exception and error message."""
     try:
         for value in values:
-            cursor.execute("INSERT" + replace + " INTO " + table + ";", value)
+            cursor.execute(statement, value)
     except Exception as e:
         print("Exception: " + str(e))
         return "Error"
-
     return "Success"
 
 
 def main():
-    cities = open_city_file()  # text file with city names used to match text in job entries
+    cities = open_city_file()
     conn, cursor = connect_db("jobs.db")
-    setup_db(cursor, "jobs(id INTEGER PRIMARY KEY, title TEXT, location TEXT, skills TEXT, "
-                     "visa TEXT, onsite TEXT, website TEXT, description TEXT)")
+    table = "jobs(id INTEGER PRIMARY KEY, title TEXT, location TEXT, skills TEXT, visa TEXT, " \
+            "onsite TEXT, website TEXT, description TEXT)"
+    setup_db(cursor, table)
     print("Connected to database.")
 
-    job_ids = get_job_ids("https://hacker-news.firebaseio.com/v0/item/21936440.json")
+    job_ids = request_comment("https://hacker-news.firebaseio.com/v0/item/21936440.json", "kids")
     if job_ids == "Invalid URL":
         print("Error with URL - shutting down.")
         exit()
@@ -170,18 +174,18 @@ def main():
     print("Retrieved job IDs from API.")
 
     job_ids = check_ids(cursor, job_ids)
-    if len(job_ids) == 0:
+    job_listings = get_listings(job_ids)
+    if len(job_listings) == 0:
         print("No new jobs to add to database. Shutting down.")
         exit()
 
-    job_listings = get_listings(job_ids)
     print("Retrieved new job postings from API.")
 
     job_listings = parse_listings(job_listings, cities)
     print("Parsed job postings.")
 
-    response = write_db(cursor, "jobs(id, title, location, skills, visa, onsite, website, description) "
-                                "VALUES(?, ?, ?, ?, ?, ?, ?, ?)", job_listings, "")
+    response = write_db(cursor, "INSERT INTO jobs(id, title, location, skills, visa, onsite, website, description) "
+                                "VALUES(?, ?, ?, ?, ?, ?, ?, ?);", job_listings)
     if response != "Error":
         print("Wrote to database. Shutting down.")
     else:
--- tests/test_Main.py
+++ tests/test_Main.py
@@ -3,8 +3,8 @@ import Main
 
 def test_get_jobs_ids():
     # ensures that a valid URL returns the IDs of job posts, and bad URL does not attempt to pull them
-    good_result = Main.get_job_ids("https://hacker-news.firebaseio.com/v0/item/21936440.json")
-    bad_result = Main.get_job_ids("http://obviously-bad-url-for-testing.json")
+    good_result = Main.request_comment("https://hacker-news.firebaseio.com/v0/item/21936440.json", "kids")
+    bad_result = Main.request_comment("http://obviously-bad-url-for-testing.json", "kids")
     assert len(good_result) > 100
     assert bad_result == "Invalid URL"
 
@@ -21,16 +21,17 @@ def test_write_db():
     """Tests that inserting data to a table works properly. Good data should be added, bad data should be rejected."""
     conn, cursor = Main.connect_db("testing.db")
     Main.setup_db(cursor, "testing(id INTEGER PRIMARY KEY, description TEXT)")
-    assert Main.write_db(cursor, "testing(id, description) VALUES(?, ?)", [[123, 'Test Description']],
-                         " OR REPLACE") == "Success"
-    assert Main.write_db(cursor, "testing(id, description) VALUES(?, ?)", [['asdf', 456]], " OR REPLACE") == "Error"
+    assert Main.write_db(cursor, "INSERT OR REPLACE INTO testing(id, description) VALUES(?, ?);",
+                         [[123, 'Test Description']]) == "Success"
+    assert Main.write_db(cursor, "INSERT OR REPLACE INTO testing(id, description) VALUES(?, ?);",
+                         [['Test Description', 456]]) == "Error"
 
 
 def test_pull_data():
     """Tests that pulling data after insertion works properly, confirming that data is saved to the db."""
     conn, cursor = Main.connect_db("testing.db")
     Main.setup_db(cursor, "testing(id INTEGER PRIMARY KEY, description TEXT)")
-    Main.write_db(cursor, "testing(id, description) VALUES(?, ?)", [[123, 'Test Description']], " OR REPLACE")
+    Main.write_db(cursor, "INSERT OR REPLACE INTO testing(id, description) VALUES(?, ?);", [[123, 'Test Description']])
     conn.commit()
     result = cursor.execute("SELECT id FROM testing;").fetchone()
     assert result == (123,)
